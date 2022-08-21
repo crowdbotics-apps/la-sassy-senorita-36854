104a886114332adb7466fbfcf516c6087f64e528
Crowdbotics <team@crowdbotics.com>
2022-08-21 22:57:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "la_sassy_senorita_36854",
+  url: "https://la_sassy_senorita_36854.botics.co",
+  api: "https://la_sassy_senorita_36854.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
